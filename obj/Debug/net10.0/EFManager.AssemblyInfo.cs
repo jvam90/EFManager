880173da9498c438738caa74b3bfe2c4932b3bab
jvam90 <jvam90@gmail.com>
2026-04-05 15:47:57 -0300

    Adiciona deploy no ec2

--- obj/Debug/net10.0/EFManager.AssemblyInfo.cs
+++ obj/Debug/net10.0/EFManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084da269d237354038498f478c13349939ddc3b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755dc7ff8436d396b3f8fbb3a959311e3ac8041d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
